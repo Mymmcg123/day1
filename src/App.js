8b438328b193b94c8b670add88a95fe3e9875889
Mark <s6104062620144@email.kmutnb.ac.th>
2021-05-24 23:24:36 +0700

    css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const  App =()=> {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+
         <Example />
     </div>
   );
--- src/Component/day1.js
+++ src/Component/day1.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import {useState} from "react";
 import { Button,Row,Container,Col } from 'react-bootstrap';
-
+import './day1.css'
+import mark from './Mark.jpg'
 
 const Example = () => {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
     return (
-        <div>
-            <p>You clicked {count} times</p>
-            <Button onClick={() => setCount(count + 1)}>
-                Click me
-            </Button>
-            <Container>
-                <Row xs={2} md={4} lg={6}>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
-                </Row>
-                <Row xs={1} md={2}>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-            </Container>
+        <div className='blockName'>
+            <img src={mark} width='200' height='200' />
+            <h2>Name :</h2>
+            <h2>Lastname :</h2>
         </div>
     );
 }
