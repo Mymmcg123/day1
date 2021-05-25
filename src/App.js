fc6468cfba4810ab5047c8cb3628deba3ad91882
Mark <s6104062620144@email.kmutnb.ac.th>
2021-05-26 01:30:50 +0700

    Do MyProject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
 import Example from "./Component/day1";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const  App =()=> {
+
   return (
     <div className="App">
-
         <Example />
     </div>
   );
--- src/Component/day1.js
+++ src/Component/day1.js
@@ -1,18 +1,124 @@
 import React from 'react'
-import {useState} from "react";
-import { Button,Row,Container,Col } from 'react-bootstrap';
+import {useState,useEffect} from "react";
+import { Form,Button } from 'react-bootstrap';
 import './day1.css'
 import mark from './Mark.jpg'
+import axios from "axios";
 
-const Example = () => {
+
+const Example = ({addPost}) => {
     // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
+    const [Name, setName] = useState(" ");
+    const [ShName,setShName] = useState(" ");
+
+    const [LastName, setLastName] = useState(" ");
+    const [ShLastName,setShLastName] = useState(" ");
+
+    const [Project,setProject] = useState(" ");
+    const [post,setPost] = useState([]);
+
+
+    const AddName = (event)=>{
+        setName(event.target.value);
+    }
+
+    const AddLastName = (event)=>{
+        setLastName(event.target.value);
+    }
+
+    const ShowName =(event)=>{
+        event.preventDefault();
+        setShName(Name);
+    }
+
+
+    const ShowLastName =(event)=>{
+        event.preventDefault();
+        setShLastName(LastName);
+    }
+
+    const AddProject = (event)=>{
+        setProject(event.target.value);
+    }
+
+    const Posts = (event) =>{
+        event.preventDefault();
+        setPost((pushPost)=>{
+            return[...post,Project]
+        })
+
+
+    }
+
+
+    useEffect(() => {
+        test()
+    } ,[])
+
+    const test = async () => {
+        const {data} = await axios.get('https://disease.sh/v3/covid-19/countries/TH')
+        console.log("Data : " + data.deaths);
+
+    }
+
     return (
-        <div className='blockName'>
-            <img src={mark} width='200' height='200' />
-            <h2>Name :</h2>
-            <h2>Lastname :</h2>
+        <div className='grid-container'>
+            <div className="grid img">
+                <img src={mark} width="300" height="380"/>
+            </div>
+            <div className="grid Name">
+                <h4>Name : {ShName}</h4>
+            </div>
+            <div className="grid LastName">
+                <h4>Lastname : {ShLastName}</h4>
+            </div>
+            <div className="grid description">
+                <h4>Description :</h4>
+            </div>
+            <div className="grid ProjectName">
+                <h4>Project Name
+                    {post.map((Arr,index)=>
+                        <div>
+                            <p>{Arr}</p>
+                        </div>
+                    )}
+                </h4>
+
+            </div>
+            <div className="grid form">
+                <Form >
+                    <Form.Group controlId="grid formBasicEmail">
+                        <div className="form-contain1">
+                            <Form.Label className="label"><h6>Name</h6></Form.Label>
+                            <div className="form-f1">
+                                <Form.Control  type="text"  placeholder="Enter your name"  value={Name} onChange={AddName}/>
+                            </div>
+                            <Button  className="button" variant="primary" onClick={ShowName}>Add</Button>
+                        </div>
+
+                        <div className="form-contain2">
+                            <Form.Label className="label2"><h6>Lastname</h6></Form.Label>
+                            <div className="form-f2">
+                                <Form.Control className="form-f" type="text" placeholder="Enter your Lastname" value={LastName} onChange={AddLastName}/>
+                            </div>
+                            <Button className="button" variant="primary" onClick={ShowLastName}>Add</Button>
+                        </div>
+
+                        <div className="form-contain3">
+                            <Form.Label className="label3"><h6>My Project</h6></Form.Label>
+                            <div className="form-f3">
+                                <Form.Control type="text" placeholder="Enter your Description" value={Project} onChange={AddProject}/>
+                            </div>
+                            <Button className="button" variant="primary" type="submit" onClick={Posts}>Add</Button>
+                        </div>
+
+
+
+                    </Form.Group>
+                </Form>
+            </div>
         </div>
+
     );
 }
 
